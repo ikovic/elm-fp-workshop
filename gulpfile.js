7ad2adef31773ff8009605ce398d25fd123f237b
Ivan Kovic <kovic.ivan@gmail.com>
2018-04-08 13:41:22 +0200

    Update gulp to copy all static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,53 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const elm = require('gulp-elm');
-const server = require('pushstate-server');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const elm = require("gulp-elm");
+const server = require("pushstate-server");
 
-const ELM_SRC = './src/**/*.elm';
-const ELM_BUNDLE = 'main.js';
-const SASS_SRC = './src/**/*.scss';
-const SASS_MAIN = './src/style.scss';
-const HTML_SRC = './static/index.html';
-const OUTPUT = './build/';
+const ELM_SRC = "./src/**/*.elm";
+const ELM_BUNDLE = "main.js";
+const SASS_SRC = "./src/**/*.scss";
+const SASS_MAIN = "./src/style.scss";
+const STATIC_FILES = "./static/*";
+const OUTPUT = "./build/";
 
-gulp.task('styles', function() {
+gulp.task("styles", function() {
   gulp
     .src(SASS_MAIN)
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(OUTPUT));
 });
 
-gulp.task('copy-index-html', function() {
-  gulp.src(HTML_SRC).pipe(gulp.dest(OUTPUT));
+gulp.task("copy-static-files", function() {
+  gulp.src(STATIC_FILES).pipe(gulp.dest(OUTPUT));
 });
 
-gulp.task('elm-init', elm.init);
+gulp.task("elm-init", elm.init);
 
-gulp.task('elm-bundle', ['elm-init'], function() {
+gulp.task("elm-bundle", ["elm-init"], function() {
   return gulp
     .src(ELM_SRC)
     .pipe(
-      elm.bundle(ELM_BUNDLE, { debug: true }).on('error', e => console.log(e))
+      elm.bundle(ELM_BUNDLE, { debug: true }).on("error", e => console.log(e))
     )
     .pipe(gulp.dest(OUTPUT));
 });
 
-gulp.task('start-server', function() {
+gulp.task("start-server", function() {
   server.start({
     port: 3000,
     directory: OUTPUT
   });
 });
 
-gulp.task('start', function() {
-  gulp.start('styles');
-  gulp.start('copy-index-html');
-  gulp.start('start-server');
-  gulp.start('elm-bundle');
-  gulp.watch(SASS_MAIN, ['styles']);
-  gulp.watch(SASS_SRC, ['styles']);
-  gulp.watch(HTML_SRC, ['copy-index-html']);
-  gulp.watch(ELM_SRC, ['elm-bundle']);
+gulp.task("start", function() {
+  gulp.start("styles");
+  gulp.start("copy-static-files");
+  gulp.start("start-server");
+  gulp.start("elm-bundle");
+  gulp.watch(SASS_MAIN, ["styles"]);
+  gulp.watch(SASS_SRC, ["styles"]);
+  gulp.watch(STATIC_FILES, ["copy-static-files"]);
+  gulp.watch(ELM_SRC, ["elm-bundle"]);
 });
