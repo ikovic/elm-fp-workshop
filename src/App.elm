cc6af3aaa723390979c3ba6f5f0938426b543e84
Ivan Kovic <kovic.ivan@gmail.com>
2018-05-01 22:02:32 +0200

    List filtering

--- src/App.elm
+++ src/App.elm
@@ -1,30 +1,83 @@
 module App exposing (..)
 
 import Http
-import Html exposing (div, img, p, text)
-import Html.Attributes exposing (class, src)
+import Html exposing (div, img, p, text, h3, fieldset, input, label)
+import Html.Attributes exposing (class, src, type_, checked)
+import Html.Events exposing (onClick)
 import Json.Decode as Decode exposing (field, list, map4, Decoder, int, string, float)
 
 
 type alias Movie =
-    { poster : String, rating : Float, title : String, year : Int }
+    { poster : String, rating : Float, title : String, year : Int, genre : List String }
 
 
 type alias Model =
     { movies : List Movie
+    , genreFilter : List String
     , error : String
     }
 
 
 type Msg
     = LoadMovies (Result Http.Error (List Movie))
+    | ToggleGenreFilter String
 
 
-view : Model -> Html.Html msg
+updateGenreFilter : String -> List String -> List String
+updateGenreFilter filterValue filters =
+    if List.member filterValue filters then
+        List.filter (\filter -> filter /= filterValue) filters
+    else
+        filterValue :: filters
+
+
+genreFilterValues : List String
+genreFilterValues =
+    [ "Animation", "Horror", "Comedy", "Drama", "Musical", "Crime", "Thriller", "Action", "Sci-Fi", "Fantasy", "Adventure" ]
+
+
+filterMovie : Movie -> List String -> Bool
+filterMovie movie genreFilter =
+    List.any (\genre -> List.member genre genreFilter) movie.genre
+
+
+getFilteredMovies :
+    List Movie
+    -> List String
+    -> List Movie
+getFilteredMovies movies genreFilter =
+    case genreFilter of
+        [] ->
+            movies
+
+        _ ->
+            List.filter (\movie -> filterMovie movie genreFilter) movies
+
+
+view : Model -> Html.Html Msg
 view model =
-    div []
-        [ div [ class "cardHolder" ] (List.map renderCard model.movies)
+    div [ class "layout" ]
+        [ renderGenreFilter model.genreFilter
+        , div
+            [ class "cardHolder" ]
+            (List.map renderCard (getFilteredMovies model.movies model.genreFilter))
         , p [] [ text model.error ]
+        ]
+
+
+renderCheckbox : String -> Bool -> Html.Html Msg
+renderCheckbox name isChecked =
+    label []
+        [ input [ type_ "checkbox", checked isChecked, onClick (ToggleGenreFilter name) ] []
+        , text name
+        ]
+
+
+renderGenreFilter : List String -> Html.Html Msg
+renderGenreFilter genres =
+    div [ class "genreHolder" ]
+        [ h3 [] [ text "Filter by Genre" ]
+        , fieldset [ class "checkboxes" ] (List.map (\filterValue -> (renderCheckbox filterValue False)) genreFilterValues)
         ]
 
 
@@ -33,7 +86,7 @@ renderCard movie =
     div [ class "card" ]
         [ img [ src movie.poster ]
             []
-        , div [ class "card-data" ]
+        , div [ class "cardData" ]
             [ p [ class "title" ]
                 [ text movie.title ]
             , p [ class "year" ]
@@ -53,6 +106,9 @@ update msg model =
         LoadMovies (Err loadError) ->
             ( { model | error = toString loadError }, Cmd.none )
 
+        ToggleGenreFilter filterValue ->
+            ( { model | genreFilter = (updateGenreFilter filterValue model.genreFilter) }, Cmd.none )
+
 
 main : Program Never Model Msg
 main =
@@ -66,7 +122,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { movies = [], error = "" }, getMovies )
+    ( { movies = [], genreFilter = [], error = "" }, getMovies )
 
 
 getMovies : Cmd Msg
@@ -81,8 +137,9 @@ movieListDecoder =
 
 movieDecoder : Decoder Movie
 movieDecoder =
-    Decode.map4 Movie
+    Decode.map5 Movie
         (field "poster" Decode.string)
         (field "rating" Decode.float)
         (field "title" Decode.string)
         (field "year" Decode.int)
+        (field "genre" (Decode.list Decode.string))
